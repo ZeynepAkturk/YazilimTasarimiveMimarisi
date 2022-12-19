b0084d3c7fe93c93ceb673590b64b818fb953231
Zeynep Akturk <zeynepakturk.maibordo@gmail.com>
2022-12-19 12:33:46 +0300

    Facade Deseniyle ilgili ornekler eklenmistir.

---
+++ FacadeDeseni_Ornek0/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FacadeDeseni_Ornek0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Facade facade = new Facade();
+            facade.KaraListeUyeEkle("Zeynep");
+            facade.KaraListeUyeEkle("Ayşe");
+            facade.KaraListeUyeEkle("Ali");
+            facade.Sistem2UyeEkle("Beyza");
+        }
+    }
+    public class Sistem1
+    {
+        private string uye;
+        private List<string> karaListe;
+
+        public string getUye() { return uye; }
+        public void setUye(string uye) { this.uye = uye; }
+
+        public List<string> getKaraListe() { return karaListe; }
+        public void setKaraListe(string uye) { karaListe.Add(uye); }
+
+
+
+        public Sistem1(string uye)
+        {
+            setUye(uye);
+            karaListe = new List<string>();
+        }
+
+        public void KaraListeUyeEkle(string uye)
+        {
+            setKaraListe(uye);
+        }
+
+        public bool KaraListeKontrol(string uye)
+        {
+            bool onay = true;
+            if (karaListe != null)
+                foreach (string uyeler in karaListe)
+                {
+                    if (uye == uyeler)
+                    {
+                        onay = false;
+
+                    }
+
+                }
+            return onay;
+        }
+
+    }
+    public class Sistem2
+    {
+        private string uye;
+
+        public string getUye() { return uye; }
+        public void setUye(string uye) { this.uye = uye; }
+
+
+        public void uyeOl(string uye)
+        {
+            setUye(uye);
+        }
+    }
+
+    public class Kimlik
+    {
+        public bool KimlikDogrulama(string uye)
+        {
+            return true;
+        }
+    }
+
+    public class Facade
+    {
+        Sistem1 sistem1 = new Sistem1("Voldemort");
+        Sistem2 sistem2 = new Sistem2();
+        Kimlik kimlik = new Kimlik();
+
+        public void KaraListeUyeEkle(string uye)
+        {
+            sistem1.KaraListeUyeEkle(uye);
+        }
+
+
+        public void Sistem2UyeEkle(string uye)
+        {
+            if (sistem1.KaraListeKontrol(uye) && kimlik.KimlikDogrulama(uye))
+            {
+                sistem2.uyeOl(uye);
+                Console.WriteLine("Sistem2 ye üye eklenmiştir.");
+            }
+            else
+                Console.WriteLine("Sisteme üye eklenememiştir.");
+        }
+    }
+}
---
+++ FacadeDeseni_Ornek1/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace FacadeDeseni_Ornek1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Banka banka = new Banka();
+            banka.EftYap();
+            banka.krediKullan();
+        }
+    }
+    public class EFT
+    {
+        public void EftYap()
+        {
+            Console.WriteLine("Eft işlemi gerçekleştirildi.");
+        }
+    }
+    public class Kredi
+    {
+        public void KrediKullan()
+        {
+            Console.WriteLine("Kredi işlemi gerçekleştirildi.");
+        }
+    }
+    public class Bakiye
+    {
+        public bool BakiyeKontrol()
+        {
+            return false;
+        }
+
+    }
+    public class Bloke
+    {
+        public bool BlokeKontrol()
+        {
+            return false;
+        }
+
+    }
+    public class Banka
+    {
+        Kredi kredi = new Kredi();
+        EFT eft = new EFT();
+        Bloke bloke = new Bloke();
+        Bakiye bakiye = new Bakiye();
+
+
+        public void krediKullan()
+        {
+            if (bakiye.BakiyeKontrol() && (!bloke.BlokeKontrol()))
+            {
+                kredi.KrediKullan();
+            }
+            else
+                Console.WriteLine("Kredi kullanımı onaylanmamıştır.");
+        }
+
+        public void EftYap()
+        {
+            if (bakiye.BakiyeKontrol() && (!bloke.BlokeKontrol()))
+            {
+                eft.EftYap();
+            }
+            else
+                Console.WriteLine("Eft kullanımı onaylanmamıştır.");
+        }
+    }
+}
