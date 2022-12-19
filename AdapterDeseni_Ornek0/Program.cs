67c039f9f85dec3afebcb4b9f3ab022d0aff75db
Zeynep Akturk <zeynepakturk.maibordo@gmail.com>
2022-12-19 20:55:55 +0300

    Adapter Deseni ornekleri eklenmistir.

---
+++ AdapterDeseni_Ornek0/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+namespace AdapterDeseni_Ornek0
+{
+    /*Önceden yazılmış iskontolu tutarı hesaplayan bir tane sınıfımız olsun.Bu sınıfımızı tutar hesaplama
+    uygulamamıza entegre etmeye çalışalım*/
+    class Program
+    {
+        static void Main(string[] args)
+        {
+    
+            ITutar adapter = new Adapter(1000, 20);
+
+            Console.WriteLine("Tutar: " + adapter.TutarHesapla());
+           
+        }
+    }
+
+
+    public class IskontoluTutar
+    {
+        private float normalFiyat;
+        private float indirimOrani;
+
+        public float getNormalFiyat() { return normalFiyat; }
+        public void setNormalFiyat(float fiyat) { normalFiyat = fiyat; }
+
+        public float getİndirimOrani() { return indirimOrani; }
+
+        public void setİndirimOrani(float indirim) { if ((indirimOrani >= 0) && (indirimOrani <= 100))
+                                                   { indirimOrani = indirim; } 
+                                                   else
+                                                   { Console.WriteLine("Geçerli bir değer giriniz!");
+                                                      
+                                                   } 
+                                                 }
+
+        public IskontoluTutar(float fiyat, float indirim)
+        {
+            setNormalFiyat(fiyat);
+            setİndirimOrani(indirim);
+        }
+
+        public float İskontoluTutarHesapla()
+        {
+            float iskontoluFiyat = (getNormalFiyat() * ((100 - getİndirimOrani()) / 100));
+            return iskontoluFiyat;
+        }
+
+    }
+
+    public interface ITutar
+    {
+       float TutarHesapla();
+    }
+
+    public class Tutar : ITutar
+    {
+        private float fiyat;
+        public float getFiyat() { return fiyat; }
+        public void setFiyat(float fiyat) { this.fiyat = fiyat; }
+
+
+        
+        public float TutarHesapla()
+        {
+            return getFiyat();
+        }
+    }
+
+    public class Adapter : ITutar
+    {
+        IskontoluTutar iskontoluTutar;
+        
+        public Adapter(float fiyat, int indirim)
+        {
+            iskontoluTutar = new IskontoluTutar(fiyat, indirim);
+        }
+        public float TutarHesapla()
+        {
+           return iskontoluTutar.İskontoluTutarHesapla();
+        }
+    }
+
+
+}
---
+++ AdapterDeseni_Ornek1/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace AdapterDeseni_Ornek1
+{
+    class Program
+    {
+        /*Varsayalım ki bir Müzik mağazamız var ve sadece enstruman olarak gitar satışı yapıyoruz.
+          Ancak sonradan mağazamızda yeni bir enstrüman satmaya karar verdik.
+          İşte bu noktada sadece gitar siparişi verebildiğimiz class’ımıza Kemence siparişimizi de adapte ediyoruz.*/
+        static void Main(string[] args)
+        {
+            IMuzikMagazasi muzikMagazasi = new KemenceAdapter();
+            muzikMagazasi.SiparisVer("yayli");
+            
+        }
+    }
+
+    public interface IMuzikMagazasi
+    {
+        public void SiparisVer(string tur);
+    }
+
+    public class GitarSiparis : IMuzikMagazasi
+    {
+        private string gitarTuru;
+        public string getGitarTuru() { return gitarTuru; }
+        public void setGitarTuru(string gitarTuru) { this.gitarTuru = gitarTuru; }
+        
+        public void SiparisVer(string tur)
+        {
+            setGitarTuru(tur);
+            Console.WriteLine(getGitarTuru() + "siparis verilmiştir.");
+        }
+    }
+
+    public class KemenceSiparis
+    {
+
+        private string kemenceTuru;
+        public string getKemenceTuru() { return kemenceTuru; }
+        public void setKemenceTuru(string kemenceTuru) { this.kemenceTuru = kemenceTuru; }
+      
+        public void KemenceSiparisVer(string KemenceTuru)
+
+        {
+            setKemenceTuru(KemenceTuru);
+            Console.WriteLine(getKemenceTuru() + " siparis verilmiştir.");
+        }
+    }
+
+    public class KemenceAdapter : IMuzikMagazasi
+    {
+        KemenceSiparis kemenceSiparis;
+
+        public KemenceAdapter()
+        {
+            kemenceSiparis = new KemenceSiparis();
+        }
+        public void SiparisVer(string tur)
+        {
+            
+            kemenceSiparis.KemenceSiparisVer(tur);
+
+        }
+    }
+
+   
+
+}
