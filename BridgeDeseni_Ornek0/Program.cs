a46b6438455cb2b3a6d3241c2864c6079168c258
Zeynep Akturk <zeynepakturk.maibordo@gmail.com>
2022-12-20 22:43:45 +0300

    Bridge (Kopru) tasarim deseni ornekleri eklenmistir.

---
+++ BridgeDeseni_Ornek0/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace BridgeDeseni_Ornek0
+{
+    class Program
+    {
+        /* Uygulamamız belirli bir şablonda rapor hazırlayan (satış ve çalışan 
+           performans raporları gibi) ve bu raporları da farklı formatlarda (Web 
+           Formatı ya da masaüstü formatı gibi) sisteme kaydeden bir işleve 
+           sahip olsun. Böyle bir durumda sınıf diyagramı nasıl tasarlanır? */
+        static void Main(string[] args)
+        {
+            IRapor rapor = new CalisanRaporu();
+            rapor.raporOlustur(new Web());
+        }
+    }
+
+    public interface IRapor
+    {
+       
+        public void raporOlustur(IFormat format);
+    }
+
+    public class CalisanRaporu : IRapor
+    {
+        public void raporOlustur(IFormat format)
+        {
+            Console.WriteLine("Çalışan raporu oluşturuldu.");
+            format.Kaydet();
+        }
+    }
+    public class SatisRaporu : IRapor
+    {
+        public void raporOlustur(IFormat format)
+        {
+            Console.WriteLine("Satış raporu oluşturuldu.");
+            format.Kaydet();
+        }
+    }
+    public class PerformansRaporu : IRapor
+    {
+        public void raporOlustur(IFormat format)
+        {
+            Console.WriteLine("Performans raporu oluşturuldu.");
+            format.Kaydet();
+        }
+    }
+
+    public interface IFormat
+    {
+        public void Kaydet();
+    }
+
+    public class Masaustu : IFormat
+    {
+        public void Kaydet()
+        {
+            Console.WriteLine("Masaüstü formatında kaydedildi.");
+        }
+    }
+    public class Web : IFormat
+    {
+        public void Kaydet()
+        {
+            Console.WriteLine("Web formatında kaydedildi.");
+        }
+    }
+}
---
+++ BridgeDeseni_Ornek1/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace BridgeDeseni_Ornek1
+{
+    class Program
+    {
+
+        /*  Uygulamada veri tabanı yapısı ele alınsın.
+            Uygulama birden fazla veri tabanı desteği veriyor 
+            olsun ve execute, connection açma gibi operasyonlar 
+            her veri tabanı için farklı olsun
+        */
+        static void Main(string[] args)
+        {
+            Veritabani veritabani = new Veritabani1(new Oracle());
+            veritabani.Baglan();
+            veritabani.Uygula();
+        }
+    }
+
+    public interface IVeriTabani
+    {
+        public void uygula();
+        public void baglantiAc();
+
+    }
+    public class Oracle : IVeriTabani
+    {
+        public void baglantiAc()
+        {
+            Console.WriteLine("Oracle ile bağlantı gerçekleştirildi.");
+        }
+
+        public void uygula()
+        {
+            Console.WriteLine("Oracle veritabanı uygulandı.");
+        }
+    }
+
+    public class MySql:IVeriTabani
+    {
+        public void baglantiAc()
+        {
+            Console.WriteLine("MySql ile bağlantı gerçekleştirildi.");
+        }
+
+        public void uygula()
+        {
+            Console.WriteLine("MySql veritabanı uygulandı.");
+        }
+
+    }
+
+    public abstract class Veritabani
+    {
+        IVeriTabani veritabani;
+
+        public Veritabani(IVeriTabani veritab)
+        {
+            veritabani = veritab;
+        }
+        public IVeriTabani GetVeriTabani() { return veritabani; }
+        
+        public abstract void Baglan();
+        public abstract void Uygula();
+    }
+    public class Veritabani1 : Veritabani
+    {
+        public Veritabani1(IVeriTabani veritab) : base(veritab) { }
+        public override void Baglan()
+        {
+            GetVeriTabani().baglantiAc();
+        }
+
+        public override void Uygula()
+        {
+            GetVeriTabani().uygula();
+        }
+    }
+
+}
